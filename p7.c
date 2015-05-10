e170e177645065a6ddce2245b91fe0361ec952a7
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-05-10 17:13:50 +0100

    Final solution for P7

---
+++ p7.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/*
+ *	By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13
+ *	We can see that the 6th prime is 13.
+ *	What is the 10 001st prime number?
+ *
+ */
+ 
+typedef enum { false, true } bool;
+
+// Take a number and a list of primes < number.
+// If one prime divides number then number is not prime.
+// Else number is prime.
+
+bool prime_check(long* array, int length, long number){
+	int j;
+	for (j = 0; j < length && array[j] != 0; j++){
+		if (number % array[j] == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(void){
+	int limit;
+	printf("Enter the limit ");
+	scanf("%d", &limit);
+	long array[limit];
+	int i;
+	for (i = 0; i < limit; i++){
+		array[i] = 0;
+	}
+	array[0] = 2;
+	
+	long number;
+	for (i = 1; i < limit; i++){
+		for(number = array[i-1] + 1; number < 2*array[i-1]; number++){
+			if (prime_check(array, limit, number) == true){
+				array[i] = number;
+				break;
+			}
+		}
+	}
+
+	printf("The %dth prime is %ld \n",limit , array[limit-1]);
+	return 0;
+}
