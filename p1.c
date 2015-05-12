ef00fc209f137c50f71c50ef99a720da8d38b101
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-05-12 20:27:14 +0100

    Final solution to P1

--- p1.c
+++ p1.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-#include <math.h
+
+int main(){
+	int result = 0;
+	for (int i = 0; i < 1000; i++){
+		result += (i % 3 == 0) ? i : (i % 5 == 0) ? i : 0;
+	}
+	printf("Result: %d \n", result);
+}
