19b346bdb7d4132f904a381bec743c05ff878c70
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-05-12 20:41:45 +0100

    Final solution to P3

--- p3.c
+++ p3.c
@@ -2,15 +2,13 @@
 #include <math.h>
 
 int main(){
-	long number;
-	printf("Enter number: \n");
-	scanf("%ld", &number);
+	long number = 600851475143;
 	int i;
-	for (i = 0; i <= number; i++){
+	for (i = 1; i <= number; i++){
 		if (number % i == 0){
 			number = number / i;
 		}
 	}
-	printf("Largest prime factor: %ld \n", i);
+	printf("Largest prime factor: %d \n", i - 1);
 	return 0;
 }
