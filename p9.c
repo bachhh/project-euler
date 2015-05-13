e6bb402f3ca515349f705abf071c7803d2ba9165
Xuan DO <xuando@carolle-linux.cs.ucl.ac.uk>
2015-05-13 09:44:56 +0100

    Incomplete P9

---
+++ p9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    int array_c[500], array_a[334], array_b[500];
+    for (int i = 0; i < 500; i++){
+        array_c[i] = i*i;
+        array_b[i] = i*i;
+    }
+    for (int i = 0; i < 334; i++){
+        array_a[334] =  i * i;
+    }
+    
+}
