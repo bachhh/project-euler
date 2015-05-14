35d184d6aad8b608c7f77582091fb83edb9283f1
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-05-14 22:11:02 +0100

    Solution to P9

--- p9.c
+++ p9.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 /* 
  *
  *  A Pythagorean triplet is a set of three natural numbers 
@@ -12,27 +13,30 @@
  *  Find the product abc.
  *
  */
- 
-/*          
- *  ### Math ###
- *  (a + b) = 1000 - c
- *  (a + b)^2 = (1000 - c)^2
- *  a^2 + b^2 + 2ab = 1000^2 - 2000c + c^2, also: a^2 + b^2 = c^2
- *  ab/500 = 1000 - 2c
- *  a b are natural number -> ab/500 > 0 -> 1000 -2c > 0 -> c < 500
- *   0 < c < 500 -> 1000 - 2c belongs to N -> ab % 500 == 0
+
+
+/*			Background:
+ *
+ *		a + b + c = 1000
+ *	and a^2 + b^2 = c^2 
+ * 
+ * 	gives ab/500 = 1000-2c, since a b c are positive integers
+ *	ab > 0 => 1000-2c > 0
+ *	
+ *	thus a < b < c < 500
+ *		 ab mod 5 = 0 
  *
  */
- 
 
 int binary_search(int* array,int length, int key){
-    int l = 0; r = length;
+    int l = 0; 
+	int r = length;
     int pivot = (l + r) / 2;
     while( l <= r){
         if ( key < array[pivot]){
             r = pivot-1, pivot = (l + r)/2;
         }
-        else if ( key > array[pivot]{
+        else if ( key > array[pivot]){
             l = pivot+1, pivot = (l + r)/2;
         }
         else{
@@ -43,21 +47,27 @@ int binary_search(int* array,int length, int key){
 }
 
 int main(){
-    int a, b, c;
-    int array_c[500], array_a[334], array_b[500];
-    for (int i = 0; i < 500; i++){
-        array_c[i] = i*i;
-        array_b[i] = i*i;
-    }
-    for (int i = 0; i < 334; i++){
-        array_a[334] =  i * i;
-    }
+    int a, b, c, array_c[500], array_a[500], array_b[500];
     
-    for (a = 0;  a < 334; a++){
-        for (b = 500/a; b < 500 && b 
+    // 3 arrays of perfect square.
+    for (int i = 0; i < 500; i++){
+    	array_a[i] = array_b[i] = array_c[i] = i*i;
     }
-        
-        
-        
-        
+
+    for (a = 1;  a <= 500; a++){
+        for (int i = a*(a+1)/500; i < a; i++){
+        	// if i <= (a^2/500) then b <= a
+    		// if i >= a then b => 500 > c
+        	int ab = 500*i; 
+        	if (ab % a != 0 ){
+        		continue;
+        	}
+        	b = ab / a;
+    		c = binary_search(array_c, 500, array_a[a] + array_b[b]);
+    		if (c != -1 && a + b + c == 1000){
+    			goto found;
+    		}
+        }
+    }
+    found: printf("Product a*b*c = %d \n", a*b*c);
 }
