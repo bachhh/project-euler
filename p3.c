f6caf79f9a4ffb2ab4bf62c5e5f721f2c44667e1
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-05-15 10:02:51 +0100

    Final solution to P3

--- p3.c
+++ p3.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+ *	The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ *	What is the largest prime factor of the number 600851475143 ?
+ *
+ */
+ 
 int main(){
 	long number = 600851475143;
 	int i;
