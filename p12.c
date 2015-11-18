d21e58d00a864ef9fb50d0c9e1271ce3c3aee634
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-11-18 18:57:56 +0000

    final solution to q12

--- p12.c
+++ p12.c
@@ -1,45 +1,68 @@
 #include <stdio.h>
-#define COUNT 1000000
+#include <stdlib.h>
+#define COUNT 100000000
 
 
 /* 
- *      Honestly I've discovered how to use the index of array as an 
- * integer value itself, here's my  things 
+ *  Honestly I've discovered how to use the index of array as an 
+ * integer value itself, here's my augmented Sieve of Eratothense
+ *  
+ *  Each index are the largest prime factor of that index. Or itself
+ * if the index is prime.
  * 
  */
+
+
+int divisors(long int number, long int* sieve){
+    if ( number > COUNT) printf("ERROR! number out of sieve range!\n");
+    if ( number % 2 == 0) number = number/2;
+    long int divider = number;
+    int divisors = 1;
+    int prevfactor = sieve[divider];
+    int exponent = 1;
+    while(divider > 1){
+        int factor = sieve[divider];
+        divider = divider/sieve[divider];
+        if ( factor != prevfactor){
+            divisors *= exponent;
+            exponent = 1; // reset exponent
+        }
+        exponent++;
+        prevfactor = factor;
+    }
+    divisors *= exponent;
+    return divisors;
+    
+}
+
 int main(){
-    int sieve[COUNT];
-    for (int i = 0; i < COUNT; i++) sieve[i] = i;
-    for (int i = 2; i < (COUNT/2); i++){
+    // Augmented Sieve
+    long int *sieve = malloc(COUNT*sizeof(long int));
+    int primecount = 0;
+    for (long int i = 0; i < COUNT; i++) sieve[i] = i;
+    for (long int i = 2; i < (COUNT/2); i++){
         if(sieve[i] == i){
-            int multiples = 2*i;
+            primecount++;
+            long int multiples = 2*i;
             do{
                 sieve[multiples] = i;
                 multiples += i;
             } while(multiples < COUNT);
         }
     }
-    for (int i = 1; i < COUNT; i++) if(sieve[i] == 0) printf("Zero value %d \n", i);
     
-        long long int triangular = 1; // initial
-        int n = 1;           // 7th triangular number
-        while(1){
-            int factors[COUNT] = {0};
-            int divider = triangular;
-            do {
-                int factor = sieve[divider];
-                if (factor > 1) factors[factor]++;
-                divider = divider/factor;
-            } while (divider > 1);
-            
-            int cardinality = 1;
-            for (int i = 0; i < COUNT; i++){
-                if(factors[i] > 0) cardinality *= (factors[i] + 1);
-            }
-            if(cardinality >= 500) break;
-            n++;
-            triangular += n;
-        }
-        printf("Triangular = %lld \n", triangular);
+    long int prevN = 1, nextN = 2;
+    int prevNdivisors = divisors(prevN, sieve);
+    int nextNdivisors = divisors(nextN, sieve);
+    int totaldivisors = prevNdivisors * nextNdivisors;
+    while (totaldivisors < 502){
+        prevN = nextN;
+        prevNdivisors = nextNdivisors;
+        nextN++;
+        nextNdivisors = divisors(nextN, sieve);
+        totaldivisors = prevNdivisors * nextNdivisors;
+    }
+    printf("Triangular = %ld \n", prevN*nextN/2);
+    
     return 0;
 }
