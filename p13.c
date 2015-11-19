3e3964e1c89be31ce244aeb0f784fd762b29f0bd
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-11-19 19:11:10 +0000

    p13 bug fix

--- p13.c
+++ p13.c
@@ -6,7 +6,7 @@ int main(){
     
     char buffer[1000];
     file_ptr = fopen("p13.txt", "r");
-    if (file_ptr) {
+    if (!file_ptr) {
         printf("ERROR! no file found!\n");
         return 1;
     }
@@ -14,13 +14,25 @@ int main(){
     int matrix[50][100];
     int r = 0, c = 0;
     while ( fgets(buffer,1000, file_ptr) != NULL){
-        c = 0;
-        while(buffer[c] != '\n'){
-            matrix[c][r] = atoi(buffer[c]);
+        c = 0; 
+        while(buffer[c] != '\n' && buffer[c] != '\000'){
+            matrix[c][r] = buffer[c] - '0';
+            c++;
         }
-        c++;
+        r++;
+    }
+    int temp = 0, result[10], digit = 9;
+    for (int c = 49; c > 39 ; c--){
+        for ( int r = 0; r < 100; r++){
+            temp += matrix[c][r];
+        }
+        result[digit] = temp % 10;
+        temp = temp/10;
+        digit--;
+        
     }
     
+    for (int i = 0; i < 10; i++) printf("%d", result[i]);
     fclose(file_ptr);
         
     return 0;
