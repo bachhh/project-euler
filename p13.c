91a57d16f01e242244b7ce1643fe022ce803c3eb
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-11-20 17:46:34 +0000

    Final solution to p13

--- p13.c
+++ p13.c
@@ -15,24 +15,31 @@ int main(){
     int r = 0, c = 0;
     while ( fgets(buffer,1000, file_ptr) != NULL){
         c = 0; 
-        while(buffer[c] != '\n' && buffer[c] != '\000'){
+        while(c<50){
             matrix[c][r] = buffer[c] - '0';
             c++;
         }
         r++;
     }
-    int temp = 0, result[10], digit = 9;
-    for (int c = 49; c > 39 ; c--){
+    
+    for (int r =0; r< 100; r++){
+        for (int c = 0; c < 50; c++)
+            printf("%d", matrix[c][r]);
+        printf("\n");
+    }
+    
+    int temp = 0, result[50];
+    for (int digit = c = 49; c >= 0  ; c--){
         for ( int r = 0; r < 100; r++){
             temp += matrix[c][r];
         }
-        result[digit] = temp % 10;
+        result[c] = temp % 10;
         temp = temp/10;
-        digit--;
-        
     }
     
+    printf("\ntemp =  %d", temp);
     for (int i = 0; i < 10; i++) printf("%d", result[i]);
+    printf("\ntemp =  %d \n", temp);
     fclose(file_ptr);
         
     return 0;
