dcaf65eb4fd46c2df6456cecbb5bc13a0b2b4cec
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-12-10 02:45:28 +0000

    Final solution to p16 Added .gitignore

--- p16.c
+++ p16.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-    unsigned long long int n = 1; 
-    while(n< 100000000000000000){
-        printf(" %lld \n", n);
-        n *= 2;
-        
+    int n[1000] = {0}; 
+    n[0] = 2;
+    int remain = 0;
+    for (int i=0; i<999; i++){
+        for (int j=0; j<1000; j++){
+            int temp = n[j]*2 + remain;
+            remain = temp/10;
+            n[j] = temp%10;
+            
+        }
+        remain = 0;
     }
+    int result = 0;
+    for(int i = 0; i < 1000; i++) result+= n[i];
+    printf("%d", result);
     return 0;
 }
