dce5c18e27efc83a7926f6be69d44ff316efe0d7
k-i-k-ichi <k-i-k-ichi@users.noreply.github.com>
2015-12-12 20:12:59 +0000

    Final solution p17

--- p17.c
+++ p17.c
@@ -33,12 +33,29 @@ char* names[28] = {
   };
   
 int convert(char** names,int i){
-    if (i < 20) return strlen(names[i]);
-    else if ( i <= 99) return convert(names,i%10) + strlen(names[i/10+18]);
-    
+    if (i < 20) {
+        return strlen(names[i]);
+    }
+    else if ( i <= 99){
+        return convert(names,i%10) + strlen(names[i/10+18]);
+    }
+    else if (i <= 999){
+        int temp = strlen(names[i/100]) + strlen("hundred");
+        if (i%100!=0) 
+            temp = temp + strlen("and") + convert(names,i%100); 
+        return temp;
+    }
+    else{
+        return strlen("onethousand");
+    }
 }
-
 int main(){
+    // Sum from 1 -> 99
+    int hundredsum=0;
+    int i=1001;
+    while(i-->0) 
+        hundredsum += convert(names,i);
+    printf("%d \n", hundredsum);
     
     return 0;
 }
